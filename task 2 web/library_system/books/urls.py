270665d56d7ed496a0b4e4be269eb928fae7bc64
Kadyrbay Batyrkhan <batyrhan.ka2004@gmail.com>
2025-03-31 02:16:01 +0500

    8

--- task 2 web/library_system/books/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import book_list, unavailable_books
-
-urlpatterns = [
-    path('books/', book_list, name='book_list'),
-    path('books/unavailable-books/', unavailable_books, name='unavailable_books'),
-]
--- task 2 web/library_system/library_system/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect  # Добавляем импорт
-
-urlpatterns = [
-    path('', lambda request: redirect('book_list')),  # Перенаправление на список книг
-    path('books/', include('books.urls')),  # Подключение маршрутов из books
-    path('admin/', admin.site.urls),
-]
